cdcce315bc070ce22ccb8747dfe3f6001ff686a2
Topher <tophersymps@gmail.com>
2018-05-16 17:33:44 -0400

    A little more tinkering

---
+++ OAuthAttempt/mySQL/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/register', function(req, res, next) {
+  res.render('index', { title: 'Registration Complete'});
+});
+
+module.exports = router;
