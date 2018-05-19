ecd58ceaa760972b82e87ae6884014bad55f47b9
Topher <tophersymps@gmail.com>
2018-05-19 15:59:51 -0400

    Fix syntax

--- models/books.js
+++ models/books.js
@@ -2,11 +2,12 @@ module.exports = function(sequelize, Sequelize) {
  
     var Books = sequelize.define('books', {
  
+        id: {
             autoIncrement: true,
             primaryKey: true,
             type: Sequelize.INTEGER
         },
-
+    
         google_id: {
             type: Sequelize.STRING,
         },
